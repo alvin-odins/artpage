f66411bf422f7e5247b260a1558009c58fa4e3b0
alvin <alvin.odins@gmail.com>
2017-04-20 00:44:57 -0700

    fix modal

--- app.js
+++ app.js
@@ -8,6 +8,27 @@ $(document).ready(function(){
 		
 		$(this).text('Liked')
 			.closest('.flask-item')
-			.addClass('is-liked');
-	})
-})
+			.toggleClass('is-liked');
+	});
+	
+	$('.js-add-link').on('click', function(event){
+		event.preventDefault();
+		
+		$('.js-form').toggleClass('is-visible');
+	});
+	
+	// 
+	//  modal
+	//
+	$('.js-show-modal').on('click', function(event) {
+    event.preventDefault();
+
+    $('.js-modal').addClass('is-visible');
+    $('.js-modal-overlay').addClass('is-visible');
+  });
+	
+	$('.js-modal-overlay').on('click', function(event) {
+    $('.js-modal').removeClass('is-visible');
+    $('.js-modal-overlay').removeClass('is-visible');
+  });
+});
